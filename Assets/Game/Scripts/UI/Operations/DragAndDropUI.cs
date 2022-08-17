30f0195c3b57c75410c1f23183b8f1c0aac58776
Isaías <83733853+Jean-Delfino@users.noreply.github.com>
2022-08-17 16:34:05 -0300

    Drag and Drop
    Agora é possível fazer objetos serem Drag and Drop não importando o objeto, isso foi utilizado para mexer com as "pop-ups" de informações das estruturas.

---
+++ Assets/Game/Scripts/UI/Operations/DragAndDropUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class DragAndDropUI : MonoBehaviour
+{
+    private RectTransform thisRect;
+
+    [SerializeField] float horizontalSpeed;
+    [SerializeField] float verticalSpeed;
+
+    [SerializeField] Vector2 superiorLimit = default;
+    [SerializeField] Vector2 inferiorLimit = default;
+
+    private void Start() {
+        thisRect = transform.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(){
+        float x = horizontalSpeed * Input.GetAxis("Mouse X");
+        float y = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        thisRect.position += new Vector3(x * Time.deltaTime, y * Time.deltaTime, 0);
+        
+        thisRect.anchoredPosition = new Vector2(
+            Mathf.Clamp(thisRect.anchoredPosition.x, inferiorLimit.x, superiorLimit.x),
+            Mathf.Clamp(thisRect.anchoredPosition.y, inferiorLimit.y, superiorLimit.y)
+        );
+    }
+}
